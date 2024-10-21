82d572d8fdc6874ee6c946a8f313a63181fc1ce3
Rajeev Singh <Rajeevsingh629944@gmail.com>
2024-10-21 13:26:10 +0530

    first commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,29 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className='nav'>
       <div className="left">
-            Portfolio
+        Portfolio
       </div>
-      <div className="right">
+      <div className={`right ${isOpen ? 'open' : ''}`}>
         <ul>
-        <li>
-            <Link to="/">Home</Link>
+          <li>
+            <Link to="/" onClick={toggleMenu}>Home</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/about" onClick={toggleMenu}>About</Link>
           </li>
           <li>
-            <Link to="/project">Projects</Link>
+            <Link to="/project" onClick={toggleMenu}>Projects</Link>
           </li>
           <li>
-            <Link to="/contact">Contact me</Link>
+            <Link to="/contact" onClick={toggleMenu}>Contact me</Link>
           </li>
         </ul>
       </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
